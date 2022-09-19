493fa0fa168b495c46c1dc837a4168f63ae0c97b
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-19 23:49:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import logging
+from pyrogram import Client, filters, idle
+from pyrogram.types import *
+import requests
+import os
+import re
+import asyncio
+from datetime import datetime
+from config import *
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+API_ID = API_ID
+API_HASH = API_HASH 
+SUDO_USERS = SUDO_USERS
+
+if not STRING_SESSION:
+    logging.error("No String Session Found! Exiting!")
+    quit(1)
+
+if not API_ID:
+    logging.error("No Api-ID Found! Exiting!")
+    quit(1)
+
+if not API_HASH:
+    logging.error("No ApiHash Found! Exiting!")
+    quit(1)
