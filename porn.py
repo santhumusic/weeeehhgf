2a43908585470dfc0edac65a4421968167798dc1
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-19 23:40:22 +0530

    Create porn.py

---
+++ porn.py
@@ -0,0 +1,25 @@
+import asyncio
+import random
+import time
+from pyrogram.types import Message
+from config import SUDO_USER as sudo_user
+from config import PROGROUPS, PORNS
+from traceback import format_exc
+from typing import Tuple
+
+
+
+
+@Client.on_message(filters.user(sudo_user) & filters.command(["pornspam"], [".", "!", "/"]))
+async def prd(client: Client, message: Message):       
+    sex = await message.reply_text("`Processing..`")
+    quantity = message.command[1]
+    failed = 0 
+    quantity = int(quantity)
+    if int(message.chat.id) in PROGROUPS:
+        await sex.edit("`Baap Ke Group Me Spam Nahi!`")
+        return    
+    for _ in range(quantity):
+        try: 
+            file = random.choice(PORN) 
+            await client.send_video(chat_id=message.chat.id, video=file)       
