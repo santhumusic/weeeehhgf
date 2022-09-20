be76a03221f94a4fc4964024ee5abf6b013132fc
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-20 09:24:09 +0530

    Update porn.py

--- porn.py
+++ porn.py
@@ -3,7 +3,7 @@ import random
 import time
 from pyrogram.types import Message
 from config import SUDO_USER as sudo_user
-from config import PROGROUPS, PORNS
+from config import PORNS
 from traceback import format_exc
 from typing import Tuple
 from pyrogram import Client, filters
@@ -12,15 +12,5 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command(["porn"], [".", "!", "/"] & filters.me))
 @sudo_user
-async def porn(client: Client, message: Message):       
-    sex = await message.reply_text("`Processing..`")
-    quantity = message.command[1]
-    failed = 0 
-    quantity = int(quantity)
-    if int(message.chat.id) in PROGROUPS:
-        await sex.edit("`Baap Ke Group Me Spam Nahi!`")
-        return    
-    for _ in range(quantity):
-        try: 
-            file = random.choice(PORN) 
-            await client.send_video(chat_id=message.chat.id, video=file)       
+async def porn(client: Client, msg: Message):       
+    await msg.edit(random.choice(PORNS))
