08194f5812e90a90449005f5b0a26bcdd37c91f0
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-20 22:12:54 +0530

    Update ban.py

--- spam/ban.py
+++ spam/ban.py
@@ -1,4 +1,4 @@
-from pyrogram import Client,filters
+from pyrogram import Client, filters
 import asyncio
 from pyrogram.types import Message
 from spam import bot
@@ -16,16 +16,16 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 @Client.on_message(filters.command(["banall"], [".", "/", "!"])) 
-async def banall(bot,message):
+async def banall(client: Client, message: Message):
     logging.info("new chat {}".format(message.chat.id))
     logging.info("getting memebers from {}".format(message.chat.id))
-    a= bot.iter_chat_members(message.chat.id)
-    for i in a:
+    member = client.get_chat_members(message.chat.id)
+    for alls in member:
         try:
-            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
-            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
+            bot.kick_chat_member(chat_id =message.chat.id,user_id=alls.user.id)
+            logging.info("kicked {} from {}".format(alls.user.id,message.chat.id))
         except Exception:
-            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
+            logging.info("failed to kicked {} from {}".format(alls.user.id,message.chat.id))
             
     logging.info("process completed")
 
