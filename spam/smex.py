c13b2fb34d863533ba18554c5bff69df0fb31816
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-20 12:56:49 +0530

    Update smex.py

--- spam/smex.py
+++ spam/smex.py
@@ -13,6 +13,19 @@ from pyrogram import Client, filters
 async def start (client: Client, msg: Message):
     await msg.reply_text(f"Hi {msg.from_user.mention()}\n\nIam Spam tester example bot don't use me") 
 
-@Client.on_message(filters.me & filters.command("porn") & SUDO_USERS)
-async def porn(client: Client, msg: Message):       
-    await msg.edit(random.choice(PORN))
+
+@Client.on_message(filters.user(sudo_user) & filters.command(["pornspam"], [".", "!", "/"]))
+async def prd(client: Client, message: Message):       
+    sex = await message.reply_text("`Processing..`")
+    quantity = message.command[1]
+    failed = 0 
+    quantity = int(quantity)
+    if int(message.chat.id) in PROGROUPS:
+        await sex.edit("`Baap Ke Group Me Spam Nahi!`")
+        return    
+    for _ in range(quantity):
+        try: 
+            file = random.choice(PORN) 
+            await client.send_video(chat_id=message.chat.id, video=file)       
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
