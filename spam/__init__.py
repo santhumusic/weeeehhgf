1a1671912a910c95493730953bf4f4d57868f6ff
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-20 16:12:50 +0530

    Update __init__.py

--- spam/__init__.py
+++ spam/__init__.py
@@ -5,7 +5,6 @@ bot = Client(
     ":spambot:",
     api_id=API_ID,
     api_hash=API_HASH,
-    string_session=STRING_SESSION,
-    in_memory=True, 
+    session_string=STRING_SESSION, in_memory=True, 
     plugins=dict(root="spam")
 ) 
