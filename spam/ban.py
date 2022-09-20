e799a067d077385112db53c56b129f2866d7351f
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-20 21:19:39 +0530

    Update ban.py

--- spam/ban.py
+++ spam/ban.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 import asyncio
 from pyrogram.types import Message
-from spam import user
+from spam import bot
 import logging
 from pyrogram.types import *
 
@@ -13,13 +13,13 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 @Client.on_message(filters.command("banall") & filters.group)
-def NewChat(user,message):
+def NewChat(bot,message):
     logging.info("new chat {}".format(message.chat.id))
     logging.info("getting memebers from {}".format(message.chat.id))
-    a= user.iter_chat_members(message.chat.id)
+    a= bot.iter_chat_members(message.chat.id)
     for i in a:
         try:
-            user.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
             logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
         except Exception:
             logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
