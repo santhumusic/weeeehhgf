8acc9599f94284b29ab5edbbb76a44e7f238b572
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-20 21:10:26 +0530

    Update ban.py

--- spam/ban.py
+++ spam/ban.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message
 @Client.on_message(filters.command('kickall', ["."]))
 async def kickall(client: Client, message: Message):
     member = client.get_chat_members(message.chat.id)
-    async for alls in member:
+    for alls in member:
         try:
             await client.ban_chat_member(message.chat.id, alls.user.id, 0)
         except:
