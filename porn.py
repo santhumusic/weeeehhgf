7ae7ec7de888045e684f0164f663d5e9b345a537
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-20 09:56:23 +0530

    Update porn.py

--- porn.py
+++ porn.py
@@ -3,7 +3,8 @@ import random
 import time
 from pyrogram.types import Message
 from config import SUDO_USER as sudo_user
-from config import PORNS, API_ID, API_HASH, STRING_SESSION 
+from config import PORNS
+from main import API_ID, API_HASH, STRING_SESSION 
 from traceback import format_exc
 from typing import Tuple
 from pyrogram import Client, filters
