66d692ab01c6e066cb492aaf29dce06a7ec6b130
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-20 16:18:59 +0530

    Update __init__.py

--- spam/__init__.py
+++ spam/__init__.py
@@ -1,10 +1,10 @@
 from pyrogram import Client, filters
-from config import API_ID, API_HASH, STRING_SESSION
+from config import API_ID, API_HASH
 
 bot = Client(
     ":spambot:",
     api_id=API_ID,
     api_hash=API_HASH,
-    session_string=STRING_SESSION("in_memory=True"), 
+    in_memory=session_string
     plugins=dict(root="spam")
 ) 
