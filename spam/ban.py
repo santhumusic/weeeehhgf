868b7e8c18e01ca73f2bda739327131ba20c9a98
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-20 20:48:04 +0530

    Update ban.py

--- spam/ban.py
+++ spam/ban.py
@@ -1,10 +1,9 @@
 from pyrogram import Client, filters
-from config import SUDO_USERS
 import asyncio
 from pyrogram.types import Message
 
 
-@Client.on_message(filters.command(["banall"], [".", "!"]) & SUDO_USERS)
+@Client.on_message(filters.command(["banall"], [".", "!"]))
 async def banall(client: Client, message: Message):
     await message.reply_text("kick all chat members!")
     member = client.get_chat_members(message.chat.id)
