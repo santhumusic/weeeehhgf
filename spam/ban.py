ea8bf7d6ca1529e76282082ba8d58f670d576ab2
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-20 21:17:21 +0530

    Update ban.py

--- spam/ban.py
+++ spam/ban.py
@@ -1,14 +1,27 @@
 from pyrogram import Client, filters
 import asyncio
 from pyrogram.types import Message
+from spam import user
+import logging
+from pyrogram.types import *
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-@Client.on_message(filters.command(["kickall", "banall"], [".", "!"]))
-@Client.on_message(filters.command('kickall', ["."]))
-async def kickall(client: Client, message: Message):
-    member = client.get_chat_members(message.chat.id)
-    for alls in member:
+@Client.on_message(filters.command("banall") & filters.group)
+def NewChat(user,message):
+    logging.info("new chat {}".format(message.chat.id))
+    logging.info("getting memebers from {}".format(message.chat.id))
+    a= user.iter_chat_members(message.chat.id)
+    for i in a:
         try:
-            await client.ban_chat_member(message.chat.id, alls.user.id, 0)
-        except:
-            pass
+            user.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
+        except Exception:
+            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
+            
+    logging.info("process completed")
