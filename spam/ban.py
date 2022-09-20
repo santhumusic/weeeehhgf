b107e59ad270e2f2c0036bc892fad9d6eec59dc7
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-20 21:58:01 +0530

    Update ban.py

--- spam/ban.py
+++ spam/ban.py
@@ -16,8 +16,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def banall(bot,message):
     logging.info("new chat {}".format(message.chat.id))
     logging.info("getting memebers from {}".format(message.chat.id))
-    a = bot.iter_chat_members(message.chat.id)
-    for i in a:
+    for i in:
         try:
             bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
             logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
