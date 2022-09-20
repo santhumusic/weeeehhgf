e60d2502680ae575080901ecd06840c37b92250d
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-20 16:05:15 +0530

    Update __init__.py

--- spam/__init__.py
+++ spam/__init__.py
@@ -3,9 +3,9 @@ from config import API_ID, API_HASH, STRING_SESSION
 
 bot = Client(
     ":spambot:",
-    API_ID,
-    API_HASH,
-    STRING_SESSION=STRING_SESSION,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=STRING_SESSION,
     in_memory=True, 
     plugins=dict(root="spam")
 ) 
