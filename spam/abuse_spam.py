2dfe6f102d824e4b4a1299351b1d9be7b4bcf3cf
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-22 09:32:19 +0530

    Update abuse_spam.py

--- spam/abuse_spam.py
+++ spam/abuse_spam.py
@@ -1,1 +1,23 @@
+#telugu coders
 
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import ABUSE_SPAM
+from typing import Tuple
+import random
+import asyncio
+
+@Client.on_message(filters.command(["spam"], [".", "!", "/"]))
+async def spam(client: Client, message: Message):   
+    await message.delete() 
+    spam = await message.reply_text("**processing your spam....**")
+    await message.delete() 
+    quantity = message.command[1]
+    failed = 0 
+    quantity = int(quantity)
+    for _ in range(quantity):
+        try: 
+            text = random.choice(ABUSE_SPAM) 
+            await client.send_message(chat_id=message.chat.id, message=text)       
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
