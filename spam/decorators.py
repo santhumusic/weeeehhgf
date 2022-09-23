81d8a000ce5e993c92d913ea3b7f1b21a5c6d7db
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-23 09:41:27 +0530

    Create decorators.py

---
+++ spam/decorators.py
@@ -0,0 +1,10 @@
+from pyrogram import Client, filters
+from config import SUDO_USERS
+from typing import Callable
+
+def sudo_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+        
+    return decorator
