bdf39c8aa347705c259f3e31387bcdc69ec92792
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-23 10:39:30 +0530

    Update addsudo.py

--- spam/addsudo.py
+++ spam/addsudo.py
@@ -1,2 +1,11 @@
 from sgram import Sclient, create_filters
 
+@Sclient.on_message(filters.command(["banall", "skip"], [".", "/", "!"]))
+@sudo_users_only
+async def banall(sclient: Sclient, message: Message):
+    await message.delete()
+    chat_id = message.chat.id
+    icm = client.get_chat_members(chat_id)
+    async for member in icm:
+        string = f"/ban {member.user.mention}\n"
+        await client.send_message(chat_id, text=string)
