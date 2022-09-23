a99c25e72ce1ea26a61e4dba17d66dff9efb81bf
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-23 16:14:11 +0530

    Update addsudo.py

--- spam/addsudo.py
+++ spam/addsudo.py
@@ -19,7 +19,7 @@ async def useradd(client: Client, message: Message):
         if user.mention in SUDO_USERS:
             await message.reply_text("Added **{0}** to Sudo Users.".format(user.mention))
         return
-    if user.id in SUDO_USERS:
+    if user.mention in SUDO_USERS:
         await message.reply_text("{0} is already a sudo user.".format(user.mention))
     if user.mention in SUDO_USERS:
         await message.reply_text(
