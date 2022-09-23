4ce44fdb9656b9fa5185f6f28aa581908542fd65
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-23 11:07:54 +0530

    Update addsudo.py

--- spam/addsudo.py
+++ spam/addsudo.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from spam.decorators import sudo_users_only
 
-@Sclient.on_message(filters.command(["addsudo"], [".", "/", "!"]))
+@Client.on_message(filters.command(["addsudo"], [".", "/", "!"]))
 @sudo_users_only
 async def banall(client: Client, message: Message):
     await message.edit("adding sudo....")
