85a96fed1c16b9259571faaff5e5f844a6aa1ae5
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-09-23 11:09:17 +0530

    Update addsudo.py

--- spam/addsudo.py
+++ spam/addsudo.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
 from spam.decorators import sudo_users_only
+from pyrogram.types import Message
 
 @Client.on_message(filters.command(["addsudo"], [".", "/", "!"]))
 @sudo_users_only
